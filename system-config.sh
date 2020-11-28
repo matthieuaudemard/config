67a9aebb5d2b0c49a53532a0da3d8793ac1ada18
Matthieu Audemard <matthieu.audemard@gmail.com>
2020-11-28 19:52:45 +0100

    refact: add variables

--- system-config.sh
+++ system-config.sh
@@ -1,6 +1,10 @@
 # link for configuring zsh :
 # https://www.freecodecamp.org/news/how-to-configure-your-macos-terminal-with-zsh-like-a-pro-c0ab3f3c1156/
 
+GIT_DIR='~/git'
+ZSH_CONFIG_FILE = '~/.zshrc'
+VIM_CONFIG_FILE = '~/.vimrc'
+
 xcode-select —-install
 
 # install and configure hombebrew
@@ -8,14 +12,14 @@ xcode-select —-install
 brew update
 
 # install & update zsh
-brew install zsh
-omz update
-echo 'export HOMEBREW_NO_AUTO_UPDATE=1' >> ~/.zshrc  # disable brew formulas auto updates
-echo 'export PATH="/usr/local/sbin:$PATH"' >> ~/.zshrc
+brew install zsh \
+&& omz update \
+&& echo 'export HOMEBREW_NO_AUTO_UPDATE=1' >> $ZSH_CONFIG_FILE \
+&& echo 'export PATH="/usr/local/sbin:$PATH"' >> $ZSH_CONFIG_FILE
 
 # install git & edit gitconfig
-brew install git
-echo 'export PATH="/usr/local/opt/git/bin"' >> ~/.zshrc
+brew install git \
+&& echo 'export PATH="/usr/local/opt/git/bin"' >> $ZSH_CONFIG_FILE
 git config --global user.name "Matthieu Audemard"
 git config --global user.email "matthieu.audemard@gmail.com"
 git config --global core.editor vim
@@ -66,11 +70,12 @@ brew install --cask \
     notion
 
 # install & configure vim
-brew install vim
-echo 'export PATH="/usr/local/opt/ruby/bin:$PATH"' >> ~/.zshrc
-git clone https://github.com/dracula/vim.git ~/git/dracula-vim
+brew install vim \
+&& echo 'export PATH="/usr/local/opt/ruby/bin:$PATH"' >> $ZSH_CONFIG_FILE
+mkdir $GIT_DIR
+git clone https://github.com/dracula/vim.git `$GIT_DIR/dracula-vim`
 mkdir -p ~/.vim/pack/themes/start
-ln -s ~/git/dracula-vim ~/.vim/pack/themes/start/dracula
+ln -s `$GIT_DIR/dracula-vim` ~/.vim/pack/themes/start/dracula
 touch ~/.vimrc
 echo 'packadd! dracula' >> ~/.vimrc
 echo 'syntax enable' >> ~/.vimrc
@@ -79,22 +84,22 @@ echo 'colorscheme dracula' >> ~/.vimrc
 
 # install & configure java11
 brew install java11
-echo 'export PATH="/usr/local/opt/openjdk@11/bin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/openjdk@11/bin:$PATH"' >> $ZSH_CONFIG_FILE
 sudo ln -sfn /usr/local/opt/openjdk@11/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk-11.jdk
 
 # install & configure php7.3 + xdebug
 brew install php@7.3
-echo 'export PATH="/usr/local/opt/php@7.3/bin:$PATH"' >> ~/.zshrc
-echo 'export PATH="/usr/local/opt/php@7.3/sbin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/php@7.3/bin:$PATH"' >> $ZSH_CONFIG_FILE
+echo 'export PATH="/usr/local/opt/php@7.3/sbin:$PATH"' >> $ZSH_CONFIG_FILE
 pecl install xdebug
 
 # install & configure mysql@5.7
 brew install mysql@5.7
-echo 'export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"' >> $ZSH_CONFIG_FILE
 
 # install mongodb
 brew install mongodb-community
-echo 'export PATH="/usr/local/Cellar/mongodb-community/4.4.1/bin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/Cellar/mongodb-community/4.4.1/bin:$PATH"' >> $ZSH_CONFIG_FILE
 
 brew install \
     maven \
@@ -105,7 +110,7 @@ brew install \
 
 # install @ configure node
 brew install node@12
-echo 'export PATH="/usr/local/opt/node@12/bin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/usr/local/opt/node@12/bin:$PATH"' >> $ZSH_CONFIG_FILE
 
 brew doctor
 brew cleanup
